b7dec23d4a8a561b5d89f5046795bdfc3cd48e64
Amir Pahadi <apahadi73@gmail.com>
2020-08-20 03:04:16 -0500

    Updates

---
+++ precache-manifest.dae00be6fea0ae28b2a74a14ac7820df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337df3938e65c69fcf5de8b65230e5b0",
+    "url": "/stockly/index.html"
+  },
+  {
+    "revision": "3a2fd80576e3b540ed32",
+    "url": "/stockly/static/css/main.0ef80bbb.chunk.css"
+  },
+  {
+    "revision": "9ac4892b5d420db568a3",
+    "url": "/stockly/static/js/2.5918e9ee.chunk.js"
+  },
+  {
+    "revision": "27200a5499d660fd8253e73b0c4cf016",
+    "url": "/stockly/static/js/2.5918e9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2fd80576e3b540ed32",
+    "url": "/stockly/static/js/main.27dce7ae.chunk.js"
+  },
+  {
+    "revision": "527ec97f8ad945633251",
+    "url": "/stockly/static/js/runtime-main.b336d253.js"
+  }
+]);
